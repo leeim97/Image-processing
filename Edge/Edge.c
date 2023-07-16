277d84c866f58556c05238a04f15869350b2c0a9
leeim97 <107461780+leeim97@users.noreply.github.com>
2023-07-16 22:48:06 +0900

    Create Edge.c
    엣지 이미지 생성

---
+++ Edge/Edge.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h> 
+#pragma warning(disable:4996)
+#include <windows.h>
+#include<math.h>
+
+int main(int argc, char* argv[]) {
+
+	//double Y, Cb, I, Cr, R, G, B, Diff_Y;
+	BITMAPFILEHEADER bmpFile;
+	BITMAPINFOHEADER bmpInfo;
+	FILE* inputFile = NULL;
+	inputFile = fopen("AICenterY.bmp", "rb");
+	fread(&bmpFile, sizeof(BITMAPFILEHEADER), 1, inputFile);
+	fread(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, inputFile);
+
+	int width = bmpInfo.biWidth;
+	int height = bmpInfo.biHeight;
+	int size = bmpInfo.biSizeImage;
+	int bitCnt = bmpInfo.biBitCount;
+	int stride = (((bitCnt / 8) * width) + 3) / 4 * 4;
+	printf("W: %d(%d)\nH: %d\nS:%d\nD: %d\n\n", width, stride, height, size, bitCnt);
+
+	unsigned char* inputImg = NULL, * outputImg = NULL, * Y1 = NULL, * Y2 = NULL;
+	Y1 = (unsigned char*)calloc(size, sizeof(unsigned char));
+	Y2 = (unsigned char*)calloc(size, sizeof(unsigned char));
+	inputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
+	outputImg = (unsigned char*)calloc(size, sizeof(unsigned char));
+	fread(inputImg, sizeof(unsigned char), size, inputFile);
+
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++) {
+			Y1[j * width + i] = inputImg[j * stride + 3 * i + 0];
+
+		}
+	}
+
+	//-----------------------------------------
+	int c[3] = { -1,0,1 };
+	double Gx, Gy, G;
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++) {
+
+			if (i == 0 || j == 0 || i == width - 1 || j == height - 1) {
+				Y2[j * width + i] = 0;
+			}
+			else {
+				Gy = (c[0] * Y1[(j - 1) * width + i] + c[2] * Y1[(j + 1) * width + i]) / (c[0] * (j - 1) + c[2] * (j + 1));
+				Gx = (c[0] * Y1[j * width + (i - 1)] + c[2] * Y1[j * width + (i + 1)]) / (c[0] * (i - 1) + c[2] * (i + 1));
+				G = sqrt(Gx * Gx + Gy * Gy);
+				Y2[j * width + i] = (unsigned char)(G > 255 ? 255 : (G < 0 ? 0 : G));
+			}
+		}
+	}
+
+	//------------------------------------------
+
+
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++) {
+			outputImg[j * stride + 3 * i + 0] = Y2[j * width + i];
+			outputImg[j * stride + 3 * i + 1] = Y2[j * width + i];
+			outputImg[j * stride + 3 * i + 2] = Y2[j * width + i];
+		}
+	}
+
+
+	FILE* outputFile = fopen("AICenterY_Edge.bmp", "wb");
+	fwrite(&bmpFile, sizeof(BITMAPFILEHEADER), 1, outputFile);
+	fwrite(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, outputFile);
+	fwrite(outputImg, sizeof(unsigned char), size, outputFile);
+
+	free(outputImg);
+	free(inputImg);
+	fclose(inputFile);
+	fclose(outputFile);
+}
